d8492a26c03e5e1e06cf736bc58de48158288654
bjoern <b.irion@wtnet.de>
2026-05-16 07:54:15 +0200

    Write service functions and run tests

--- src/main/java/lombok/UniversityService.java
+++ src/main/java/lombok/UniversityService.java
@@ -1,20 +1,31 @@
 package lombok;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @NoArgsConstructor
 public class UniversityService {
 
     public double getCourseAverage(Course course) {
-        return 0.0;
+        return course.getStudents().stream()
+                .mapToDouble(Student::getGrade)
+                .average()
+                .orElse(0.0);
     }
 
     public double getUniversityAverage(University university) {
-        return 0.0;
+        return university.courses().stream()
+                .flatMap(course -> course.getStudents().stream())
+                .mapToDouble(Student::getGrade)
+                .average()
+                .orElse(0.0);
     }
 
-    public ArrayList<Student> getStudentsByGradeGood(University university) {
-        return new ArrayList<Student>();
+    public List<Student> getStudentsByGradeGood(University university) {
+        return university.courses().stream()
+                .flatMap(course -> course.getStudents().stream())
+                .filter(student -> student.getGrade() <= 2)
+                .distinct()
+                .toList();
     }
 }
